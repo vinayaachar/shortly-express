54061fa0acdbf0254a33398877cfb263118c5860
vinayaachar <vinayaachar@gmail.com>
2021-01-20 19:45:31 -0800

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,19 @@
 const parseCookies = (req, res, next) => {
+  var cookiesObj = req.cookies;
+  var cookieString = req.headers.cookie;
+
+  if (cookieString) {
+    var arrayOfCookies = cookieString.split(';');
+    arrayOfCookies.forEach(item => {
+      var arrayOfValuePairs = item.split('=');
+      arrayOfValuePairs[0] = arrayOfValuePairs[0].trim();
+      cookiesObj[arrayOfValuePairs[0]] = arrayOfValuePairs[1];
+
+    });
+  }
+  console.log('cookies', cookiesObj);
+  req.cookies = cookiesObj;
+  next();
 };
 
 module.exports = parseCookies;
